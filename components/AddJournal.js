93dc487bcbc6238572e138e7872dcdb78be4513a
Seth Childers <chi11008@byui.edu>
2018-11-14 21:08:27 -0700

    Added a form into the AddJournal component that submits a new Journal object

--- components/AddJournal.js
+++ components/AddJournal.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import {
+    Button,
     View,
+    StyleSheet,
     TextInput
 } from 'react-native';
+import { Formik } from 'formik'
 
 const AddJournal = ({ addJournal }) => {
     return (
-        <View>
-            <TextInput
-                placeholder="Enter journal title here..."
-                onSubmitEditing={event => addJournal(event.nativeEvent.text)}
-            />
-        </View>
+        <Formik 
+            // initialValues={{ title: '', content: '' }} 
+            onSubmit={formValues => addJournal(formValues)}
+        >
+            {props => (
+                <View>
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Enter journal title here..."
+                        onChangeText={props.handleChange('title')}
+                        value={props.values.title}
+                    />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Enter more stuff here..."
+                        onChangeText={props.handleChange('content')}
+                        value={props.values.content}
+                    />
+                    <Button onPress={props.handleSubmit} title="Submit" />
+                </View>
+            )}
+        </Formik>
     )
 };
+
+const styles = StyleSheet.create({
+    textInput: {
+        height: 50,
+        fontSize: 15,
+        marginLeft: 5
+    }
+});
 
 export default AddJournal;
