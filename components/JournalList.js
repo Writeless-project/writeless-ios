07d7f383599182038cb3bc3fb617a664fa1252c3
Seth Childers <chi11008@byui.edu>
2018-11-23 16:49:47 -0700

    Adding delete slider

--- components/JournalList.js
+++ components/JournalList.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Text, List, ListItem } from 'native-base';
+import Swipeout from 'react-native-swipeout';
 
 const renderList = journals => {
     if (journals) {
+        
+        // the buttons that appear when the item is swiped to the left
+        var swipeoutBtns = [{
+            text: 'Delete',
+            backgroundColor: '#dd0000'
+        }];
+
         return journals.map((journal, i) => {
             return (
-                <ListItem key={i}>
-                    <Text>
-                        {journal.title}
-                    </Text>
-                </ListItem>
+                <Swipeout key={i} right={swipeoutBtns}>
+                    <ListItem style={styles.listItem}>
+                        <Text>
+                            {journal.title}
+                        </Text>
+                    </ListItem>
+                </Swipeout>
             )
         });
     }
