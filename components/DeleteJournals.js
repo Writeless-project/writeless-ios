a110c15691602aa973a83d1f19be006f153c2241
Danverde <verde.daniel.d@gmail.com>
2018-12-07 14:42:06 -0700

    removed more unused files. Updated deleteAll button styling

--- components/DeleteJournals.js
+++ components/DeleteJournals.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import {
     Button,
-    View,
-} from 'react-native';
+    Text
+} from 'native-base';
 import { Formik } from 'formik'
 
 const DeleteJournals = ({ deleteJournals }) => {
     return (
         <Formik onSubmit={formValues => deleteJournals()}>
             {props => (
-                <View>
-                    <Button onPress={props.handleSubmit} title="Delete All Journals" />
-                </View>
+                <Button danger style={styles.button} full onPress={props.handleSubmit}>
+                    <Text>Delete All Journals</Text>
+                </Button>
             )}
         </Formik>
     )
 };
 
+/* Because the default margin-top is 200.... */
+const styles = StyleSheet.create({
+    button: {
+        marginTop: 20,
+        margin: 20,
+    }
+});
 
 export default DeleteJournals;
